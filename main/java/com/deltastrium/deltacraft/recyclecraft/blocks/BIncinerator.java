566b2104b71907e10186c92bd073d3037875684d
Letiu <ga54siw@mytum.de>
2015-03-15 17:06:34 +0100

    updated BIncinerator

--- main/java/com/deltastrium/deltacraft/recyclecraft/blocks/BIncinerator.java
+++ main/java/com/deltastrium/deltacraft/recyclecraft/blocks/BIncinerator.java
@@ -3,7 +3,10 @@ package com.deltastrium.deltacraft.recyclecraft.blocks;
 import com.deltastrium.deltacraft.recyclecraft.core.RecycleCraft;
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.init.Blocks;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 public class BIncinerator extends BlockContainer implements IRecycleCraftBlock {
@@ -15,9 +18,13 @@ public class BIncinerator extends BlockContainer implements IRecycleCraftBlock {
     }
 
     @Override
+    public IIcon getIcon(int side, int meta) {
+        // temporary
+        return Blocks.furnace.getIcon(0, 0);
+    }
+
+    @Override
     public TileEntity createNewTileEntity(World world, int meta) {
         return null;
     }
-
-
 }
