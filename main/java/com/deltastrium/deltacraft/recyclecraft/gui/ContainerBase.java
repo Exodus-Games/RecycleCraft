89624356407e8a409a67556b5babe974ee90063f
Letiu <ga54siw@mytum.de>
2015-03-16 22:51:53 +0100

    added ContainerBase

---
+++ main/java/com/deltastrium/deltacraft/recyclecraft/gui/ContainerBase.java
@@ -0,0 +1,26 @@
+package com.deltastrium.deltacraft.recyclecraft.gui;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.InventoryPlayer;
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.Slot;
+
+public abstract class ContainerBase extends Container {
+
+    protected void bindPlayerInventory(InventoryPlayer inventoryPlayer) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 9; j++) {
+                addSlotToContainer(new Slot(inventoryPlayer, j + i * 9 + 9, 8 + j * 18, 84 + i * 18));
+            }
+        }
+
+        for (int i = 0; i < 9; i++) {
+            addSlotToContainer(new Slot(inventoryPlayer, i, 8 + i * 18, 142));
+        }
+    }
+
+    @Override
+    public boolean canInteractWith(EntityPlayer player) {
+        return false;
+    }
+}
