206fb98568ab366753667798b15313223173d086
Letiu <ga54siw@mytum.de>
2015-03-17 00:20:04 +0100

    added util method for block placing

---
+++ main/java/com/deltastrium/deltacraft/recyclecraft/util/FacingUtil.java
@@ -0,0 +1,17 @@
+package com.deltastrium.deltacraft.recyclecraft.util;
+
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.util.MathHelper;
+
+public class FacingUtil {
+
+    public static int getFurnaceMeta(EntityLivingBase player) {
+        int l = MathHelper.floor_double((double) (player.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
+        switch(l) {
+            case 0: return 2;
+            case 1: return 5;
+            case 2: return 3;
+            default: return 4;
+        }
+    }
+}
