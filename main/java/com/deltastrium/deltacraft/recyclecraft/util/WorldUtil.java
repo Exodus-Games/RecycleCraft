239e0c09a53ef6a39cd9e76d7d55a216d0e08825
Letiu <ga54siw@mytum.de>
2015-03-17 12:05:21 +0100

    added wrapper method for setBit4()

--- main/java/com/deltastrium/deltacraft/recyclecraft/util/WorldUtil.java
+++ main/java/com/deltastrium/deltacraft/recyclecraft/util/WorldUtil.java
@@ -1,5 +1,6 @@
 package com.deltastrium.deltacraft.recyclecraft.util;
 
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
 
 public class WorldUtil {
@@ -8,6 +9,11 @@ public class WorldUtil {
     public static void setBit4(World world, int x, int y, int z, boolean value, int flag) {
         int meta = world.getBlockMetadata(x, y, z);
         world.setBlockMetadataWithNotify(x, y, z, value ? (meta | 8) : (meta & 7), flag);
+    }
+
+    /** Sets the last bit of metadata at the specified tileentity */
+    public static void setBit4(TileEntity tile, boolean value, int flag) {
+        setBit4(tile.getWorldObj(), tile.xCoord, tile.yCoord, tile.zCoord, value, flag);
     }
 
 }
