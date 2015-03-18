fdf4f8410918c82d49cf4882ba6f7f5bb82a60b6
Letiu <Letiu@users.noreply.github.com>
2015-03-18 20:34:32 +0100

    started getSynchedFacing()

--- main/java/com/deltastrium/deltacraft/recyclecraft/util/FacingUtil.java
+++ main/java/com/deltastrium/deltacraft/recyclecraft/util/FacingUtil.java
@@ -5,6 +5,11 @@ import net.minecraft.util.MathHelper;
 
 public class FacingUtil {
 
+    /** returns a new facing for a block depending on the given rotation example. */
+    public static int getSynchedFacing(int exampleOld, int exampleNew, int currentFacing) {
+
+    }
+
     public static int getFurnaceMeta(EntityLivingBase player) {
         int l = MathHelper.floor_double((double) (player.rotationYaw * 4.0F / 360.0F) + 0.5D) & 3;
         switch(l) {
