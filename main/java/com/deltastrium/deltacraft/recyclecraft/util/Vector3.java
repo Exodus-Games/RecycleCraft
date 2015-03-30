6985720785811eabe25394fee6eda11590ae46e1
Letiu <Letiu@users.noreply.github.com>
2015-03-30 15:55:58 +0200

    added more util methods and doc comments

--- main/java/com/deltastrium/deltacraft/recyclecraft/util/Vector3.java
+++ main/java/com/deltastrium/deltacraft/recyclecraft/util/Vector3.java
@@ -4,6 +4,8 @@ import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.Facing;
 import net.minecraftforge.common.util.Constants;
 
+import static com.deltastrium.deltacraft.recyclecraft.util.FacingUtil.findRotation;
+
 public class Vector3 {
 
     public int x, y, z;
@@ -18,6 +20,7 @@ public class Vector3 {
         this.z = z;
     }
 
+    /** Shifts the vector in the spcified direction (0,1,2,3,4,5). Modifies the current instance. */
     public void offsetForSide(int side) {
         if (side >= 0 && side <= 5) {
             this.x = Facing.offsetsXForSide[side];
@@ -26,6 +29,7 @@ public class Vector3 {
         }
     }
 
+    /** Rotates the vector on the specified axis (0,1,2,3,4,5). Returns a new instance. */
     public Vector3 rotate(int axis) {
         switch (axis) {
             case 0: return new Vector3(z, y, -x);
@@ -37,6 +41,20 @@ public class Vector3 {
         }
 
         return null;
+    }
+
+    /** Rotates the vector based ont the given rotation example. Returns a new instance. */
+    public Vector3 rotateToMeta(int oldMeta, int newMeta) {
+        Vector3 result = copy();
+        int rotation = findRotation(oldMeta, newMeta);
+
+        if (rotation == -2) return result;
+        if (rotation == -1) return result.rotate(rotation).rotate(rotation);
+        return result.rotate(rotation);
+    }
+
+    public Vector3 copy() {
+        return new Vector3(x, y, z);
     }
 
     public NBTTagCompound writeToNBT() {
@@ -57,6 +75,28 @@ public class Vector3 {
         Vector3 vector = new Vector3();
         vector.readFromNBT(compound);
         return vector;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Vector3 vector3 = (Vector3) o;
+
+        if (x != vector3.x) return false;
+        if (y != vector3.y) return false;
+        if (z != vector3.z) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        result = 31 * result + z;
+        return result;
     }
 
     public String toString() {
