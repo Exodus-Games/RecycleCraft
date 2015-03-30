cdee33c076129886962204ed2b3d48fa71dcff33
Letiu <Letiu@users.noreply.github.com>
2015-03-30 20:08:10 +0200

    added realistic compactor pattern

--- main/java/com/deltastrium/deltacraft/recyclecraft/data/MultiblockData.java
+++ main/java/com/deltastrium/deltacraft/recyclecraft/data/MultiblockData.java
@@ -9,15 +9,36 @@ public class MultiblockData {
     public static void init() {
         compactor = new Multiblock();
         compactor.setCore(BlockData.compactor, 3);
-        compactor.addPart(Blocks.planks, 1, 0, 0, 0, false);
-     /*   compactor.addPart(Blocks.planks, -1, 0, 0, 1, false);
-        compactor.addPart(Blocks.planks, 0, 1, 0, 2, false);
-        compactor.addPart(Blocks.planks, 0, -1, 0, 3, false);
-        compactor.addPart(Blocks.piston, 1, 0, 1, 4, true);
-        compactor.addPart(Blocks.piston, -1, 0, 1, 5, true);
-        compactor.addPart(Blocks.piston, 0, 1, 1, 0, true);
-        compactor.addPart(Blocks.piston, 0, -1, 1, 1, true);*/
+        compactor.addPart(Blocks.stonebrick, 1, 0, 0, 0, false);
+        compactor.addPart(Blocks.stonebrick, -1, 0, 0, 0, false);
+        compactor.addPart(Blocks.stonebrick, 1, 0, -2, 0, false);
+        compactor.addPart(Blocks.stonebrick, -1, 0, -2, 0, false);
+        compactor.addPart(Blocks.stonebrick, 1, 2, 0, 0, false);
+        compactor.addPart(Blocks.stonebrick, -1, 2, 0, 0, false);
+        compactor.addPart(Blocks.stonebrick, 1, 2, -2, 0, false);
+        compactor.addPart(Blocks.stonebrick, -1, 2, -2, 0, false);
 
+        compactor.addPart(Blocks.planks, 1, 0, -1, 0, false);
+        compactor.addPart(Blocks.planks, -1, 0, -1, 0, false);
+        compactor.addPart(Blocks.planks, 0, 0, -2, 0, false);
+
+        compactor.addPart(Blocks.planks, 1, 1, 0, 0, false);
+        compactor.addPart(Blocks.planks, -1, 1, 0, 0, false);
+        compactor.addPart(Blocks.planks, 1, 1, -2, 0, false);
+        compactor.addPart(Blocks.planks, -1, 1, -2, 0, false);
+
+        compactor.addPart(Blocks.planks, 1, 2, -1, 0, false);
+        compactor.addPart(Blocks.planks, -1, 2, -1, 0, false);
+        compactor.addPart(Blocks.planks, 0, 2, 0, 0, false);
+        compactor.addPart(Blocks.planks, 0, 2, -2, 0, false);
+
+        compactor.addPart(Blocks.piston, 1, 1, -1, 4, true);
+        compactor.addPart(Blocks.piston, -1, 1, -1, 5, true);
+        compactor.addPart(Blocks.piston, 0, 2, -1, 0, true);
+        compactor.addPart(Blocks.piston, 0, 0, -1, 1, true);
+
+        compactor.addPart(Blocks.piston, 0, 1, 0, 2, true);
+        compactor.addPart(Blocks.piston, 0, 1, -2, 3, true);
     }
 
 }
