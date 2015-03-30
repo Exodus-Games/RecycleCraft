1a46a90dd03b292794e6b09155b5912ead0f7209
Letiu <Letiu@users.noreply.github.com>
2015-03-30 19:44:54 +0200

    updated MultiblockData

--- main/java/com/deltastrium/deltacraft/recyclecraft/data/MultiblockData.java
+++ main/java/com/deltastrium/deltacraft/recyclecraft/data/MultiblockData.java
@@ -10,13 +10,13 @@ public class MultiblockData {
         compactor = new Multiblock();
         compactor.setCore(BlockData.compactor, 3);
         compactor.addPart(Blocks.planks, 1, 0, 0, 0, false);
-        compactor.addPart(Blocks.planks, -1, 0, 0, 1, false);
+     /*   compactor.addPart(Blocks.planks, -1, 0, 0, 1, false);
         compactor.addPart(Blocks.planks, 0, 1, 0, 2, false);
         compactor.addPart(Blocks.planks, 0, -1, 0, 3, false);
         compactor.addPart(Blocks.piston, 1, 0, 1, 4, true);
         compactor.addPart(Blocks.piston, -1, 0, 1, 5, true);
         compactor.addPart(Blocks.piston, 0, 1, 1, 0, true);
-        compactor.addPart(Blocks.piston, 0, -1, 1, 1, true);
+        compactor.addPart(Blocks.piston, 0, -1, 1, 1, true);*/
 
     }
 
